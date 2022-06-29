5109ef811a8eb61ab1cc0f9158c7f9901b8fe725
Maron-Wilsons <maronwilson@bk.ru>
2022-06-29 22:55:03 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './components/routes/Routes'
+import './index.css'
+
+import * as firebase from 'firebase/app'
+import { AuthProvider } from './components/providers/AuthProvider'
+
+firebase.initializeApp({
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<AuthProvider>
+			<Routes />
+		</AuthProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
